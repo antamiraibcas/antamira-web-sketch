15f3085828d1b3db8725bc78f86b017460a66ef7
Álvaro Gómez García <agg150503@gmail.com>
2021-01-24 16:10:57 +0100

    Diseño 3 Beta V1.0

---
+++ dist/js/app.js
@@ -0,0 +1,18 @@
+// // Declare HTML document parts
+// const visualizer = document.getElementById('visualizer');
+// const img = document.querySelectorAll('.box');
+// const textCarousel = document.querySelector('.text-carousel');
+// // Declare index
+// let idx=0;
+
+// function run(){
+//     idx++;
+//     if(idx == img.length){
+//         idx = 0;
+//     }
+//     visualizer.style.transform = `translateX(${-idx * 426}px)`;
+//     textCarousel.style.transform = `translateX(${-idx * 426}px)`;
+// }
+
+// setInterval(run, 10000);
+
